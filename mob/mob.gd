ca67864b5d8b48bd50e840d44dda224c2136b9d4
NISCHAY KUMAR <NischayTech@gmail.com>
2026-04-18 20:27:32 +0530

    FIXED SOME ROTATION

--- mob/mob.gd
+++ mob/mob.gd
@@ -1,6 +1,20 @@
 extends RigidBody3D
 
 @onready var bat_model: Node3D = %bat_model
+var speed = randf_range(1.0, 1.7)
+
+@onready var player = get_node("/root/Game/Player")
+
+func _physics_process(delta):
+	var target = player.global_position
+	target.y = global_position.y
+	
+	var direction = global_position.direction_to(target)
+	linear_velocity = direction * speed
+	
+	# FIXED THE ROTATION
+	bat_model.look_at(target, Vector3.UP)
+	bat_model.rotate_y(PI) # BECUZ model faces +Z
 
 func take_damage():
 	bat_model.hurt()
