d82bdbb225c0c0b4da168bdbad20c71d99f5ddca
NISCHAY KUMAR <NischayTech@gmail.com>
2026-04-19 00:26:49 +0530

    blades go brrr

--- mob/spawner/mob_spawner_3d.gd
+++ mob/spawner/mob_spawner_3d.gd
@@ -9,4 +9,4 @@ extends Node3D
 func _on_timer_timeout() -> void:
 	var new_mob = mob_to_spawn.instantiate()
 	add_child(new_mob)
-	new_mob.gloval_position = marker_3d.global_position
+	new_mob.global_position = marker_3d.global_position
